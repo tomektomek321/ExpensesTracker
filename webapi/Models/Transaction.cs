0faefb971e0730a293144ec839d30af940d0f247
Kacper Patroniak <patron944@live.com>
2023-06-08 00:06:40 +0100

    Disable implicit usings

--- webapi/Models/Transaction.cs
+++ webapi/Models/Transaction.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
 
 namespace webapi.Models {
     public class Transaction {
--- webapi/WeatherForecast.cs
+++ webapi/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace webapi;
 
 public class WeatherForecast
