cb0ce3bd3eedf6c8d4a3a678a48bb06f28f79cf5
tomektomek321 <71927596+tomektomek321@users.noreply.github.com>
2023-06-08 16:51:34 +0200

    Feature/init front app (#5)
    * Recreate front init app
    
    * add deps
    
    * Init chakra config
    
    * Basic navbar with not logged user menu
    
    * install recoil and add auth atom
    
    * Basic routes for home and categories

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './chakra/theme';
+import { Navbar } from './components/navbar/Navbar';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import MyCategories from './pages/MyCategories';
+
+function App() {
+  return (
+    <RecoilRoot>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="categories" element={<MyCategories />} />
+            </Routes>
+          </div>
+        </ChakraProvider>
+      </BrowserRouter>
+    </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ client/src/chakra/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./button";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF3C00",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  components: {
+    Button,
+  },
+});
+
+export default theme;
