b2977896a650b74d149bc7be5cff8ffabefbc554
Tomek <tomek1989gw@wp.pl>
2023-06-08 15:46:12 +0000

    install recoil and add auth atom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,17 @@ import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './chakra/theme';
 import { Navbar } from './components/navbar/Navbar';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <div className="App">
-        <Navbar />
-      </div>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <div className="App">
+          <Navbar />
+        </div>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
 
