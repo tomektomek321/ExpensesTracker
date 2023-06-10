04afbfb9ca8d5e8292fcdc59a996edf598539917
Kacper Patroniak <patron944@live.com>
2023-06-11 00:25:00 +0100

    Add Category Manager

---
+++ webapi/Managers/CategoryManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi.Managers {
+    public class CategoryManager {
+        private Data.ExpensesDbContext _context;
+        public CategoryManager(Data.ExpensesDbContext pContext) {
+            _context = pContext;
+        }
+        public IEnumerable<Models.Category> GetCategories() {
+            return _context.Categories.AsEnumerable().OrderBy(x=>x.Name);
+        }
+    }
+}
