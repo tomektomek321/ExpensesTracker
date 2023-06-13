8e618117e49fccf339ca71a6c1eb0a7372138bd3
Kacper Patroniak <patron944@live.com>
2023-06-13 09:47:17 +0100

    Update transactions manager to cater for UserId

--- webapi/Managers/TransactionsManager.cs
+++ webapi/Managers/TransactionsManager.cs
@@ -13,13 +13,18 @@ namespace webapi.Managers
         public TransactionsManager(Data.ExpensesDbContext pContext) {
             _context = pContext;
         }
-        public IEnumerable<Models.Transaction> GetTransacations(DateTime? pTransactionsDate) {
-            IOrderedEnumerable<Data.Models.Transaction> dbCategories = _context.Transactions.Where(x=>x.Date.Date == (pTransactionsDate ?? x.Date.Date)).AsEnumerable().OrderBy(x=>x.Date);
+        public IEnumerable<Models.Transaction> GetTransacations(DateTime? pTransactionsDate, string pUserId) {
+            IOrderedEnumerable<Data.Models.Transaction> dbCategories = _context.Transactions
+                    .Where(x=>
+                        x.Date.Date == (pTransactionsDate ?? x.Date.Date)
+                        && x.UserId == pUserId)
+                    .AsEnumerable()
+                    .OrderBy(x=>x.Date);
             return dbCategories.Select(x => new Models.Transaction(x)).ToList();
         }
-        public Models.Transaction AddTransaction(Models.Transaction pTransaction) {
+        public Models.Transaction AddTransaction(Models.Transaction pTransaction, string pUserId) {
             var dbTransaction = new Data.Models.Transaction();
-            dbTransaction.UserId = pTransaction.UserId;
+            dbTransaction.UserId = pUserId;
             dbTransaction.CategoryId = pTransaction.CategoryId;
             dbTransaction.Date = pTransaction.Date;
             dbTransaction.Amount = pTransaction.Amount;
@@ -28,13 +33,28 @@ namespace webapi.Managers
             _context.SaveChanges();
             return new Models.Transaction(inserted);
         }
-        public Models.Transaction GetTransactionById(Guid pCategoryId) {
-            Data.Models.Transaction? dbTransaction = _context.Transactions.Where(x=>x.Id == pCategoryId).FirstOrDefault();
+        public Models.Transaction GetTransactionById(Guid pTransactionId, string pUserId) {
+            Data.Models.Transaction? dbTransaction = _context.Transactions
+                    .Where(x=>
+                        x.Id == pTransactionId 
+                        && x.UserId == (pUserId ?? x.UserId))
+                    .FirstOrDefault();
             Models.Transaction transaction = null;
             if (dbTransaction != null) {
                 transaction = new Models.Transaction(dbTransaction);
             }
             return transaction;
+        }
+        public void DeleteTransaction(Guid pTransactionId, string pUserId) {
+            Data.Models.Transaction? dbTransaction = _context.Transactions.FirstOrDefault(x=>x.Id == pTransactionId && x.UserId == (pUserId ?? x.UserId));
+            if (null != dbTransaction) {
+                _context.Transactions.Remove(dbTransaction);
+                _context.SaveChanges();
+            }
+            else {
+                throw new KeyNotFoundException();
+            }
+                
         }
     }
 }
