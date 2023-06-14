0e01d85d1b1ebc1a1d7427dd546c614e6fe3470a
Tomek <tomek1989gw@wp.pl>
2023-06-14 13:52:17 +0000

    signUp

--- clientapp/src/common/utils/budget/common-util-budget.ts
+++ clientapp/src/common/utils/budget/common-util-budget.ts
@@ -13,14 +13,15 @@ export function countDayBudget(numberOfDaysForMonth: number, budget: number): nu
 
 export function setColorForPercentage(percentage: number): string {
   
-  if(percentage < 30) return 'green.300';
-  if(percentage < 70) return 'green.100';
-  if(percentage < 80) return 'red.100';
-  if(percentage < 90) return 'red.200';
+  if(percentage < 30) return 'green.400';
+  if(percentage < 70) return 'green.300';
+  if(percentage < 80) return 'green.200';
+  if(percentage < 90) return 'green.100';
+  if(percentage < 100) return 'red.100';
   
   if(percentage > 270) return 'red.800';
-  if(percentage < 230) return 'red.600';
-  if(percentage < 150) return 'red.500';
-  if(percentage < 130) return 'red.400';
-  return 'red.300';
+  if(percentage > 230) return 'red.600';
+  if(percentage > 150) return 'red.500';
+  if(percentage > 130) return 'red.400';
+  return 'red.100';
 }
---
+++ clientapp/src/components/modal/auth/SignUp.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { useSetRecoilState } from "recoil";
+import InputItem from "../../layout/inputs/InputItem";
+import { appState } from "../../../atoms/AppAtom";
+
+const SignUp: React.FC = () => {
+  
+  const appRecoil = useSetRecoilState(appState);
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [formError, setFormError] = useState("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (formError) setFormError("");
+    if (!form.email.includes("@")) {
+      return setFormError("Please enter a valid email");
+    }
+
+    if (form.password !== form.confirmPassword) {
+      return setFormError("Passwords do not match");
+    }
+  };
+
+  const onChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <InputItem
+        name="email"
+        placeholder="email"
+        type="text"
+        mb={2}
+        onChange={onChange}
+      />
+      <InputItem
+        name="password"
+        placeholder="password"
+        type="password"
+        mb={2}
+        onChange={onChange}
+      />
+      <InputItem
+        name="confirmPassword"
+        placeholder="confirm password"
+        type="password"
+        onChange={onChange}
+      />
+      <Button
+        width="100%"
+        height="36px"
+        mb={2}
+        mt={2}
+        type="submit"
+        isLoading={false}
+      >
+        Sign Up
+      </Button>
+      <Flex fontSize="9pt" justifyContent="center">
+        <Text mr={1}>Have an account?</Text>
+        <Text
+          color="blue.500"
+          fontWeight={700}
+          cursor="pointer"
+          onClick={() => {
+            appRecoil(prev => ({
+              ...prev,
+              viewModal: {
+                ...prev.viewModal,
+                view: "login",
+              } 
+            }))
+          }}
+        >
+          LOG IN
+        </Text>
+        {false ||
+          "Some errors?"
+        }
+      </Flex>
+    </form>
+  );
+};
+export default SignUp;
