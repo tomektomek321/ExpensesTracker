d766d84342c86e54ee7c6bc4bb2a3fd2ddb351e6
Kacper Patroniak <patron944@live.com>
2023-06-18 13:24:12 +0100

    Add worker class

---
+++ webapi/Worker.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using webapi.Data.Models;
+
+namespace webapi {
+    public static class Worker {
+        public static string ExtractUserId(ClaimsPrincipal pUser) {
+            return pUser.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
