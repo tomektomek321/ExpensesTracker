bc962c9b4de4802591136956b48d0a8f8b353930
Kacper Patroniak <patron944@live.com>
2023-06-18 13:23:02 +0100

    Renewal frequency const for subscriptions

---
+++ webapi/Data/Consts/RenewalFrequency.cs
@@ -0,0 +1,7 @@
+﻿namespace webapi.Data.Consts {
+    public class RenewalFrequency {
+        public const int WEEKLY = 1;
+        public const int MONTHLY = 2;
+        public const int YEARLY = 3;
+    }
+}
