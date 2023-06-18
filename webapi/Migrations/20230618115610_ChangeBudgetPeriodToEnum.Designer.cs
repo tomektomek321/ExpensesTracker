c46e605a0529f4d8aae2a82c4e5bbcc90b548cff
Kacper Patroniak <patron944@live.com>
2023-06-18 13:24:04 +0100

    New migrations

--- webapi/Migrations/20230618115610_ChangeBudgetPeriodToEnum.Designer.cs
+++ webapi/Migrations/20230618115610_ChangeBudgetPeriodToEnum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webapi.Data;
 
@@ -11,9 +12,11 @@ using webapi.Data;
 namespace webapi.Migrations
 {
     [DbContext(typeof(ExpensesDbContext))]
-    partial class ExpensesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230618115610_ChangeBudgetPeriodToEnum")]
+    partial class ChangeBudgetPeriodToEnum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
